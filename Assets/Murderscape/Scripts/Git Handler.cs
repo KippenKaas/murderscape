a512823d74c22de27088a45f51d76a1ab47f83f8
Luca <wafelnetwork@gmail.com>
2023-03-14 16:16:49 +0100

    'update'

---
+++ Assets/Murderscape/Scripts/Git Handler.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GitHandler : MonoBehaviour
+{
+    [MenuItem("Murderscape/InsertGit ^s")]
+    public static void InsertGit()
+    {
+        Debug.Log("Starting Insert GitHub...");
+        System.Diagnostics.Process.Start("CMD.exe", "/C git commit -a -m 'update'");
+        System.Diagnostics.Process.Start("CMD.exe", "/C git push origin master");
+    } 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
